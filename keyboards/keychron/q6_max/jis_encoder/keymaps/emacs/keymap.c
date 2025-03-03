369297e8e18ad77e2a88c7391d947b4bcb831349
applejxd <9639592+applejxd@users.noreply.github.com>
2025-03-03 22:06:08 +0900

    keymap.c: add custom keycodes for COPILOT and CHATGPT, implement website opening function

--- keyboards/keychron/q6_max/jis_encoder/keymaps/emacs/keymap.c
+++ keyboards/keychron/q6_max/jis_encoder/keymaps/emacs/keymap.c
@@ -37,7 +37,9 @@ enum custom_keycodes {
     COPY_TEXT,  
     CUT_TEXT,
     // for Windows
-    DEEPL
+    DEEPL,
+    COPILOT,
+    CHATGPT
 };
 
 /* ------ */
@@ -46,6 +48,19 @@ enum custom_keycodes {
 
 // see https://docs.qmk.fm/reference_keymap_extras#header-files
 #include <sendstring_japanese.h>
+
+void open_website(const char *url) {
+    // open command dialog
+    tap_code16(G(KC_R));
+    // wait for moving active window and clear inputs
+    SEND_STRING(SS_DELAY(300));
+    tap_code(KC_DEL);
+    // open URL
+    SEND_STRING(SS_DELAY(100));
+    send_string(url);
+    SEND_STRING(SS_DELAY(100));
+    tap_code(KC_ENT);
+}
 
 bool set_mark_active = false;  // マーク状態を保持
 uint8_t mod_state;
@@ -126,6 +141,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 tap_code(KC_ENT);
             }
             break;
+        case COPILOT:
+            if (record->event.pressed) {
+                open_website("https://m365.cloud.microsoft/chat?auth=2");
+            }
+            break;
+        case CHATGPT:
+            if (record->event.pressed) {
+                open_website("https://chatgpt.com/");
+            }
+            break;
     }
     // Keychron original macros
     // true: Process all other keycodes normally
@@ -181,19 +206,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______,      _______,     _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,  _______,  _______,                                 _______,                  _______,  _______,  _______,  _______,
         _______, _______,      _______,     _______,                      _______,                      _______,  _______,  _______,  _______,  _______,           _______,    _______,    _______,      _______,            _______          ),
     [WIN_BASE] = LAYOUT_113_jis(
-        KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,            KC_PSCR,G(S(KC_F23)),RGB_MOD, KC_F13,   KC_F14,   KC_F15,   KC_F16,
+        KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,            KC_PSCR,  CHATGPT,  RGB_MOD,  RGB_HUI,  RGB_SAI,  RGB_VAI,  RGB_SPI,
         KC_GRV,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_INT3,  KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_ENT,             KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
         MO(WIN_FN),KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_BSLS,                                                      KC_P4,    KC_P5,    KC_P6,
         KC_LSFT,   KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_INT1,  KC_RSFT,                                  KC_UP,              KC_P1,    KC_P2,    KC_P3,    KC_PENT,
         KC_LCTL,   KC_LGUI,  KC_LALT,  KC_LNG2,                      KC_SPC,                       KC_LNG1,  KC_RGUI,   KC_RALT, MO(MAC_FN), KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT          ),
     [WIN_FN] = LAYOUT_113_jis(
-        _______, KC_BRID,      KC_BRIU,     KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,    KC_MNXT,   KC_MUTE,     KC_VOLD,     KC_VOLU,    RGB_TOG,            G(KC_G),    G(S(KC_F23)), RGB_RMOD,    RGB_SPD,    RGB_SPI,  RGB_HUD,  RGB_HUI,
-        _______, BT_HST1,      BT_HST2,     BT_HST3,  P2P4G,    _______,  _______,  _______, _______,    _______,   _______,     _______,     _______,    _______,   _______, _______,    _______,      _______,     _______,    _______,  _______,  _______,
-        RGB_TOG, G(S(KC_F23)), CUT_TEXT,    KC_END,   C(KC_R),  C(KC_T),  C(KC_V),  C(KC_Z), KC_TAB,     C(KC_O),   KC_UP,       SET_MARK,    C(KC_RBRC), C(KC_ENT),          _______,    _______,      _______,     _______,    _______,  _______,  _______,
-        _______, KC_HOME,      C(KC_F),     KC_DEL,   KC_RGHT,  ABORT,    KC_BSPC,  KC_ENT,  CUT_LINE,   C(KC_L),   C(KC_SCLN),  C(KC_QUOT),  C(KC_BSLS),                                                            _______,    _______,  _______,
-     C(KC_LSFT), G(KC_DOWN),   OSL(WIN_CX), C(KC_C),  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_ENT,  C(KC_COMM), C(KC_DOT), C(KC_Z),     _______,     C(KC_RSFT),                                 KC_MS_UP,                  _______,    _______,  _______,  _______,
-        _______, C(KC_LGUI),   C(KC_LALT),  _______,                      SET_MARK,                      _______,   C(KC_RCMMD), C(KC_ROPTN), _______,    KC_MS_BTN1,         KC_MS_LEFT, KC_MS_DOWN,   KC_MS_RIGHT, KC_MS_BTN2,           _______          ),
+        _______, KC_BRID,      KC_BRIU,     KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV, KC_MPLY,    KC_MNXT,   KC_MUTE,     KC_VOLD,     KC_VOLU,    RGB_TOG,            G(KC_G),    COPILOT,    RGB_RMOD,    RGB_HUD,    RGB_SAD,  RGB_VAD,  RGB_SPD,
+        _______, BT_HST1,      BT_HST2,     BT_HST3,  P2P4G,    _______,  _______,  _______, _______,    _______,   _______,     _______,     _______,    _______,   _______, _______,    _______,    _______,     _______,    _______,  _______,  _______,
+        RGB_TOG, G(S(KC_F23)), CUT_TEXT,    KC_END,   C(KC_R),  C(KC_T),  C(KC_V),  C(KC_Z), KC_TAB,     C(KC_O),   KC_UP,       SET_MARK,    C(KC_RBRC), C(KC_ENT),          _______,    _______,    _______,     _______,    _______,  _______,  _______,
+        _______, KC_HOME,      C(KC_F),     KC_DEL,   KC_RGHT,  ABORT,    KC_BSPC,  KC_ENT,  CUT_LINE,   C(KC_L),   C(KC_SCLN),  C(KC_QUOT),  C(KC_BSLS),                                                          _______,    _______,  _______,
+     C(KC_LSFT), G(KC_DOWN),   OSL(WIN_CX), C(KC_C),  KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_ENT,  C(KC_COMM), C(KC_DOT), C(KC_Z),     _______,     C(KC_RSFT),                                 KC_MS_UP,                _______,    _______,  _______,  _______,
+        _______, C(KC_LGUI),   C(KC_LALT),  _______,                      SET_MARK,                      _______,   C(KC_RCMMD), C(KC_ROPTN), _______,    KC_MS_BTN1,         KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_MS_BTN2,           _______          ),
     [MAC_CX] = LAYOUT_113_jis(
         _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,
         _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
